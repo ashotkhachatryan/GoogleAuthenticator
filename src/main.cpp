cd403d06a4e5d3c81cdfa46d881c26b9365b64fa
Ashot Khachatryan <ashot@Ashots-MacBook-Air-2.local>
2023-03-26 00:31:31 +0900

    Provide Client Secret file as a command line argument

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,12 @@
 #include "ClientSecret.h"
 #include "GoogleAuthenticator.h"
 
-int main() {
-    auto secret = ClientSecret::FromJson(R"");
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cout << "Please provide client secrets json file path\n";
+        return -1;
+    }
+    auto secret = ClientSecret::FromJson(argv[1]);
     auto credentials = GoogleAuthenticator::Authenticate(secret);
     if (credentials) {
         std::cout << credentials->access_token << '\n';
