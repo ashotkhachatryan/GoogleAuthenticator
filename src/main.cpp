eb237e10d3a4b8da56082502ecdacd59f5a79001
Ashot Khachatryan <ashotkhachatryanh@gmail.com>
2023-04-05 23:52:56 +0900

    Added an example to get files list from Google Drive

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "ClientSecret.h"
 #include "GoogleAuthenticator.h"
+#include "httplib.h"
+#include "constants.h"
 
 int main(int argc, char** argv) {
     if (argc < 2) {
@@ -10,6 +12,12 @@ int main(int argc, char** argv) {
     auto secret = ClientSecret::FromJson(argv[1]);
     auto credentials = GoogleAuthenticator::Authenticate(secret);
     if (credentials) {
-        std::cout << credentials->access_token << '\n';
+        httplib::SSLClient cli(GAPI_URL);
+        // Google Drive files list under root
+        auto res = cli.Get(FILES_URL + "?q='root'%20in%20parents",
+                           {{"Authorization", "Bearer " + credentials->access_token }});
+        if (res.error() == httplib::Error::Success) {
+            std::cout << res.value().body << std::endl;
+        }
     }
 }
